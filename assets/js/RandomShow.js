c78c6abdbb70e6b0c8daf710970917344ccc8acb
author <don952509@foxmail.com>
2021-07-14 19:32:16 +0800

    girl: author <don952509@foxmail.com>

---
+++ assets/js/RandomShow.js
@@ -0,0 +1,24 @@
+//¿ªÊ¼
+jQuery(document).ready(function ($) {
+    showMe(Random())
+    function Random(){return new Date().getSeconds()%2}
+    function showMe(r){
+        var r;
+        if (r==1){
+            L2Dwidget.init({        
+            "pluginRootPath": "live2dw_shizuku/",
+            "pluginJsPath": "lib/",
+            "pluginModelPath": "assets/",
+            "tagMode": false,
+            "debug": false,
+            "model": {"jsonPath": "/assets/live2dw/live2dw_shizuku/assets/shizuku.model.json"},
+            "display": {"position": "right", "width": 90, "height": 150, "hOffset": 100, "vOffset": -10,},
+            "mobile": {"show": true, "scale": 0.8,},
+            "log": false});
+        }
+        if (r==0) {
+            var obj=document.getElementById("abc")
+            obj.style.display=""
+        }
+    }
+});
